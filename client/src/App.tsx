a1f82b1bcb4053d543de42d4ca4e074b375d9a4f
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-02-20 13:25:19 +0900

    :recycle: tailwindcss postcss configure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { EditorView } from "./components/EditorView";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <EditorView />
     </div>
   );
